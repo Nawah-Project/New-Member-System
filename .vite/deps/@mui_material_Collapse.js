102578c2b7da428e7bb822dda3a26a11738adb24
AbdoEssam2024 <eabdo2051@gmail.com>
2025-11-22 13:18:44 +0200

    Delete .vite/deps directory

--- .vite/deps/@mui_material_Collapse.js
+++
@@ -1,8 +0,0 @@
-import "./react-BaqBjuxQ.js";
-import "./DefaultPropsProvider-B9QAhXBu.js";
-import "./createSvgIcon-CFxLgTrp.js";
-import { Collapse_default, collapseClasses_default, getCollapseUtilityClass } from "./Collapse-DeqkqObr.js";
-import "./chainPropTypes-DUgDBFoG.js";
-import "./react-dom-BlejFq9i.js";
-
-export { collapseClasses_default as collapseClasses, Collapse_default as default, getCollapseUtilityClass };
