55275fbba93e009247b3a26c3e1d42cbe2e831b8
AbdoEssam2024 <eabdo2051@gmail.com>
2025-12-02 23:09:26 +0200

    Enhancing Files

--- src/const/footer/Footer.jsx
+++ src/const/footer/Footer.jsx
@@ -3,7 +3,6 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import XIcon from '@mui/icons-material/X';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
-import './footer.css'
 import { Link } from 'react-router-dom';
 export default function Footer() {
     return (
